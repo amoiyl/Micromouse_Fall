04779b845ae9942274ac66a6001268ca73134137
amoizhao <zhaoa6125@gmail.com>
2023-11-04 21:23:32 -0700

    11/04/2023
    assignment 2a incomplete

---
+++ Core/Inc/encoders.h
@@ -0,0 +1,12 @@
+/*
+ * encoders.h
+ */
+
+#ifndef INC_ENCODERS_H_
+#define INC_ENCODERS_H_
+
+int16_t getRightEncoderCounts();
+int16_t getLeftEncoderCounts();
+void resetEncoders();
+
+#endif /* INC_ENCODERS_H_ */
---
+++ Core/Src/encoders.c
@@ -0,0 +1,28 @@
+/*
+ * encoders.c
+ */
+
+#include "main.h"
+#include "encoders.h"
+
+/*
+ * Implement this function so it returns the right encoder value
+ */
+int16_t getRightEncoderCounts() {
+	return TIM2->CNT;
+}
+
+/*
+ * Implement this function so it returns the left encoder value
+ */
+int16_t getLeftEncoderCounts() {
+	return TIM1->CNT;
+}
+
+/*
+ * This function has already been implemented for you. Enjoy! :)
+ */
+void resetEncoders() {
+	TIM1->CNT = (int16_t) 0;
+	TIM2->CNT = (int16_t) 0;
+}
